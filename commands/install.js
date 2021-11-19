970d7f531d17cd147a6ab529f55834d1b22db2f6
Jerem Dlcn <44464165+JeremDlcn@users.noreply.github.com>
2021-11-19 15:17:59 +0100

    💬 update new command content

--- commands/install.js
+++ commands/install.js
@@ -1,7 +1,7 @@
 const chalk = require('chalk');
 
 function install(name) {
-    console.log(chalk.green.bold('Installation réussi'));
+    console.log(chalk.green.bold(`Installation réussie de votre projet "${name}"`));
 }
 
 module.exports = install;
