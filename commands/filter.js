5fbdf710323414cbb23c92b1bbdcd4a01e0bf0e9
Jerem Dlcn <44464165+JeremDlcn@users.noreply.github.com>
2021-11-19 15:48:59 +0100

    🚧 Adding a filter

--- commands/filter.js
+++ commands/filter.js
@@ -1,11 +1,13 @@
 const chalk = require('chalk');
 const fs = require('fs');
+const filterContent = 'module.exports = (input) => {\n\t"test";\n};'
 
 function addFilter(name) {
     try {
-        fs.writeFile(name, )
-
-        console.log(chalk.green.bold(`Le filtre "${name}" a été ajouté`));
+        fs.writeFile(`./filters/${name}.js`, filterContent, (e)=> {
+            if (e) throw (e);
+            console.log(chalk.green.bold(`Le filtre "${name}" a été ajouté`));
+        })
     } catch (error) {
         console.log(chalk.red.bold(`Le filtre "${name}" n'a pas pu être crée à cause d'une erreur`));    
     }
