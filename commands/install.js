e934560ad5582d780ffe889e90c34897c7b81be5
Auriane Labille <45030838+Fanghienne@users.noreply.github.com>
2021-11-19 15:24:57 +0100

    ppft command

--- commands/install.js
+++ commands/install.js
@@ -1,7 +1,7 @@
 const chalk = require('chalk');
 
 function install(name) {
-    console.log(chalk.green.bold('Installation réussi'));
+    console.log(chalk.green.bold('Le projet ${name} a été installé avec succès'));
 }
 
 module.exports = install;
