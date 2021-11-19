45c8f13ad66ffe8942300ee58b3b47900b66c9fa
Jerem Dlcn <44464165+JeremDlcn@users.noreply.github.com>
2021-11-19 16:24:09 +0100

    🚧 Adding a step

--- commands/step.js
+++ commands/step.js
@@ -1,7 +1,19 @@
 const chalk = require('chalk');
+const fs = require('fs');
 
 function addStep(name) {
-    console.log(chalk.green.bold(`Le step "${name}" a été ajouté`));
+    try {
+        let text = fs.readFileSync(`config-filters.json`, 'utf-8', (e)=> { if (e) throw (e); });
+        let obj = JSON.parse(text);
+        obj.steps[3] = { filter: `${name}.js`, params: [], next: '2'}
+
+        console.log(obj);
+
+
+        console.log(chalk.green.bold(`Le step "${name}" a été ajouté`));
+    } catch (error) {
+        console.log(chalk.red.bold(`Le step "${name}" n'a pas pu être ajouté à cause d'une erreur`));    
+    }
 }
 function deleteStep(name) {
     console.log(chalk.red.bold(`Le step "${name}" a été supprimé`));
